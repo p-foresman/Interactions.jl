e8c60863cccf09fa06770a9d88ebe79f0233a26f
p-foresman <98059656+p-foresman@users.noreply.github.com>
2025-06-19 18:07:57 -0700

    removed 'model' from all function parameters in simulation engine

--- src/Simulate/engine.jl
+++ src/Simulate/engine.jl
@@ -4,7 +4,7 @@
 #state is only mutated within the scope of this function (using multiple dispatch for to optimize main simulation loop)
 function simulate!(state::State, ::Nothing, ::Nothing; stopping_condition_reached::Function)
     while true
-        run_period!(state.model, state)
+        run_period!(state)
         if stopping_condition_reached(state)
             Interactions.complete!(state)
             break
@@ -16,7 +16,7 @@ end
 
 function simulate!(state::State, timeout::Int, ::Nothing; stopping_condition_reached::Function, start_time::Float64)
     while true
-        run_period!(state.model, state)
+        run_period!(state)
         if stopping_condition_reached(state)
             Interactions.complete!(state)
             break
@@ -31,7 +31,7 @@ end
 
 function simulate!(state::State, ::Nothing, db_push_period::Int; stopping_condition_reached::Function)
     while true
-        run_period!(state.model, state)
+        run_period!(state)
         if stopping_condition_reached(state)
             Interactions.complete!(state)
             break
@@ -45,7 +45,7 @@ end
 
 function simulate!(state::State, timeout::Int, db_push_period::Int; stopping_condition_reached::Function, start_time::Float64)
     while true
-        run_period!(state.model, state)
+        run_period!(state)
         if stopping_condition_reached(state)
             Interactions.complete!(state)
             break
@@ -63,14 +63,14 @@ end
 
 
 ######################## game algorithm ####################
-function run_period!(model::Model, state::State)
+function run_period!(state::State)
     for component in Interactions.components(state) #each connected component plays its own period's worth of matches
         # mpp = matches_per_period(num_vertices(component)) * edge_density(num_vertices(component), λ(graph_parameters(model))) #NOTE: CHANGE THIS BACK
         # for _ in 1:Int(ceil(mpp))
         for _ in 1:matches_per_period(component)
             Interactions.reset_arrays!(state)
             Interactions.set_players!(state, component)
-            play_game!(model, state)
+            play_game!(state)
         end
     end
     Interactions.increment_period!(state)
@@ -79,14 +79,14 @@ end
 
 
 
-function play_game!(model::Model, state::State)
+function play_game!(state::State)
     #if a player has no memories and/or no memories of the opponents 'tag' type, their opponent_strategy_recollections entry will be a Tuple of zeros.
     #this will cause their opponent_strategy_probs to also be a Tuple of zeros, giving the player no "insight" while playing the game.
     #since the player's expected utility list will then all be equal (zeros), the player makes a random choice.
     find_opponent_strategy_probabilities!(state)
-    calculate_expected_utilities!(model, state)
-    make_choices!(model, state)
-    push_memories!(model, state)
+    calculate_expected_utilities!(state)
+    make_choices!(state)
+    push_memories!(state)
     return nothing
 end
 
@@ -108,21 +108,21 @@ function calculate_opponent_strategy_probabilities!(state::State, player_number:
 end
 
 
-function calculate_expected_utilities!(model::Model, state::State)
+function calculate_expected_utilities!(state::State)
     @inbounds for column in axes(payoff_matrix(model), 2) #column strategies #NOTE: could just do 1:size(model, dim=2) or something. might be a bit faster
-        for row in axes(payoff_matrix(model), 1) #row strategies
-            Interactions.increment_expected_utilities!(state, 1, row, payoff_matrix(model)[row, column][1] * Interactions.opponent_strategy_probabilities(state, 1, column))
-            Interactions.increment_expected_utilities!(state, 2, column, Interactions.payoff_matrix(model)[row, column][2] * Interactions.opponent_strategy_probabilities(state, 2, row))
+        for row in axes(payoff_matrix(state.model), 1) #row strategies
+            Interactions.increment_expected_utilities!(state, 1, row, payoff_matrix(state.model)[row, column][1] * Interactions.opponent_strategy_probabilities(state, 1, column))
+            Interactions.increment_expected_utilities!(state, 2, column, Interactions.payoff_matrix(state.model)[row, column][2] * Interactions.opponent_strategy_probabilities(state, 2, row))
         end
     end
     return nothing
 end
 
 
-function make_choices!(model::Model, state::State)
+function make_choices!(state::State)
     for player_number in 1:2 #eachindex(model.pre_allocated_arrays.players)
         Interactions.rational_choice!(Interactions.players(state, player_number), maximum_strategy(Interactions.expected_utilities(state, player_number)))
-        Interactions.choice!(Interactions.players(state, player_number), rand() < Interactions.error_rate(model) ? Interactions.random_strategy(model, player_number) : Interactions.rational_choice(Interactions.players(state, player_number)))
+        Interactions.choice!(Interactions.players(state, player_number), rand() < Interactions.error_rate(state.model) ? Interactions.random_strategy(state.model, player_number) : Interactions.rational_choice(Interactions.players(state, player_number)))
     end
 end
 
@@ -135,9 +135,9 @@ function push_memory!(agent::Agent, percept::Interactions.Percept, memory_length
 end
 
 
-function push_memories!(model::Model, state::State)
-    push_memory!(Interactions.players(state, 1), Interactions.choice(Interactions.players(state, 2)), memory_length(model))
-    push_memory!(Interactions.players(state, 2), Interactions.choice(Interactions.players(state, 1)), memory_length(model))
+function push_memories!(state::State)
+    push_memory!(Interactions.players(state, 1), Interactions.choice(Interactions.players(state, 2)), memory_length(state.model))
+    push_memory!(Interactions.players(state, 2), Interactions.choice(Interactions.players(state, 1)), memory_length(state.model))
     return nothing
 end
 
